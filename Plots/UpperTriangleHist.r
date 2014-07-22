7350f5dd64bbe9b4cf0316390bc8b36f70b029cd
gumglegee <gz4have@hotmail.com>
2014-07-21 20:50:12 -0400

    add parameters to hist()

--- Plots/UpperTriangleHist.r
+++ Plots/UpperTriangleHist.r
@@ -4,6 +4,7 @@ args <- commandArgs()
 
 distMatrix <- args[6]
 nnode <- as.numeric(args[7])
+outfile <- args[8]
 
 #read the file into a matrix
 mi <- matrix(scan(distMatrix), nnode, nnode+1, byrow=TRUE)
@@ -14,5 +15,12 @@ mi <- mi[, -1]
 #Extract the upper triangular part in to a vector
 upp <- mi[upper.tri(mi)]
 
+pdf(outfile)
+
 #draw the histgram
-hist(upp)
+hist(upp, 
+	right=FALSE, # intervals closed on the left
+	main="Histogram of Distance", # the main title
+	xlab="Distance")	# x-axis label
+
+dev.off()
